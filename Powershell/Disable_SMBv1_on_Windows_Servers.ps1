2dc9bd720c7e0d4498d314a029b02152127f72e5
Patrick Olvera <patrickolvera@protonmail.com>
2024-03-14 08:27:54 -0500

    Added Script for 2012

--- Powershell/Disable_SMBv1_on_Windows_Servers.ps1
+++ Powershell/Disable_SMBv1_on_Windows_Servers.ps1
@@ -1,4 +1,4 @@
-# Disable SMBv1 on Windows Servers #
+# Disable SMBv1 on Windows Servers 2016 and Newer #
 
 # SMBv1 Object
 $SMBv1 = Get-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
@@ -13,4 +13,23 @@ if($SMBv1.State -eq "Disabled") {
         # Disable SMBv1
         Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
     }
+}
+
+
+# For Windows Server 2012 #
+
+# SMBv1 Feature Name
+$featureName = "FS-SMB1"
+
+# Check if SMBv1 is installed
+if ((Get-WindowsFeature -Name $featureName).InstallState -eq "Installed") {
+    #SMBv2 Object Check
+    $SMB2ProtocolEnabled = (Get-SmbServerConfiguration).EnableSMB2Protocol
+    if ($SMB2ProtocolEnabled) {
+        # Disable SMBv1
+        Remove-WindowsFeature -Name $featureName
+        Write-Output "SMBv1 has been disabled."
+    }
+} else {
+    Write-Output "SMBv1 is already Disabled"
 }
