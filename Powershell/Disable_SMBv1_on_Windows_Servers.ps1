1c282782b0d631830fc6cd0383b99827f215e83f
Patrick Olvera <patrickolvera@protonmail.com>
2024-04-11 09:05:43 -0500

    Added Disable SMB Null Sessions

--- Powershell/Disable_SMBv1_on_Windows_Servers.ps1
+++ Powershell/Disable_SMBv1_on_Windows_Servers.ps1
@@ -1,4 +1,5 @@
-# Disable SMBv1 on Windows Servers 2016 and Newer #
+#----- Disable SMBv1 on Windows Servers 2016 and Newer -----#
+# Detect: Get-WindowsOptionalFeature -Online -FeatureName SMB1Protocol #
 
 # SMBv1 Object
 $SMBv1 = Get-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
@@ -6,14 +7,20 @@ $SMBv1 = Get-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
 # Detect if SMBv1 exists
 if($SMBv1.State -eq "Disabled") {
     Write-Output "SMBv1 is already Disabled"
-} else {
-    #SMBv2 Object Check
-    $SMB2ProtocolEnabled = (Get-SmbServerConfiguration).EnableSMB2Protocol
-    if ($SMB2ProtocolEnabled) {
-        # Disable SMBv1
-        Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
-    }
+} else { 
+    # Disable SMBv1
+    Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
 }
+
+# Disable SMB Null Sessions
+
+New-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name RestrictAnonymous -Value 1 -PropertyType DWORD -Force
+New-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name RestrictAnonymousSAM -Value 1 -PropertyType DWORD -Force
+New-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name EveryoneIncludesAnonymous -Value 0 -PropertyType DWORD -Force
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters" -Name "NullSessionPipes" -Value @()
+
+
+##############################################################################################
 
 
 # For Windows Server 2012 #
