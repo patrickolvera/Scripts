d77a4c5f47ffe47fd3bf72c00e8c10308feb94ba
Patrick Olvera <PatrickOlvera@protonmail.com>
2024-07-09 10:08:30 -0500

    Added feature to install if 7zip is missing

--- Powershell/upgrade_7zip.ps1
+++ Powershell/upgrade_7zip.ps1
@@ -1,4 +1,4 @@
-#!#> Script to replace the currently installed 7zip with the latest version <!#!#
+#!#> Script to replace the currently installed 7zip with the latest version or install if 7zip is missing <!#!#
 
 # Grab the URL for the latest version of 7zip as of 7/5/2024
 $downloadurl = 'https://7-zip.org/' + (
@@ -11,28 +11,45 @@ $downloadurl = 'https://7-zip.org/' + (
 
 # Grab the latest version from $downloadurl and assign to $latestver
 if ($downloadurl -match '7z(\d+)-x64\.exe') {
-    $latestver = [int]$Matches[1]
+    $latestver = $Matches[1]
 }
 
-# Get the installed version of 7zip
-$installedver = ((Get-Package -Name '7-zip*').version -replace '\.', '')
+# Function to install 7zip
+function Install-7zip {
+    param (
+        [string]$downloadurl,
+        [string]$installerPath
+    )
 
-# Check if the installed version is less than the latest version
-if ([int]$installedver -lt $latestver) {
-    # Get the GUID of the installed 7zip
-    $7zip = $installedPrograms | Where-Object { $_.PSChildName -match "7-Zip" }
-    $7zipGUID = $7zip.PSChildName
-
-    # Uninstall the current version of 7zip
-    Start-Process -FilePath "msiexec.exe" -ArgumentList "/X $7zipGUID /quiet /norestart" -Wait -PassThru
-
-    # Install the latest version
-    $installerPath = Join-Path $env:TEMP (Split-Path $downloadurl -Leaf)
     Invoke-WebRequest $downloadurl -OutFile $installerPath
     Start-Process -FilePath $installerPath -ArgumentList "/S" -Wait -NoNewWindow
     Remove-Item $installerPath -Force
+    Write-Output "7zip installed to version $latestver"
+}
 
-    Write-Output "7zip updated to version $latestver"
+# Check if 7zip is installed by querying the registry
+$installed7zip = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" | 
+    Where-Object { $_.DisplayName -like "7-Zip*" } |
+    Select-Object -First 1
+
+if ($installed7zip) {
+    # Get the installed version of 7zip
+    $installedver = ($installed7zip.DisplayVersion -replace '\.', '')
+
+    # Check if the installed version is less than the latest version
+    if ([int]$installedver -lt [int]$latestver) {
+        # Uninstall the current version of 7zip
+        $uninstallString = $installed7zip.QuietUninstallString
+        Start-Process -FilePath "cmd.exe" -ArgumentList "/c $uninstallString" -Wait -NoNewWindow
+        
+        # Install the latest version
+        $installerPath = Join-Path $env:TEMP (Split-Path $downloadurl -Leaf)
+        Install-7zip -downloadurl $downloadurl -installerPath $installerPath
+    } else {
+        Write-Output '7zip is on the latest version'
+    }
 } else {
-    Write-Output '7zip is on the latest version'
+    # Install 7zip if not installed
+    $installerPath = Join-Path $env:TEMP (Split-Path $downloadurl -Leaf)
+    Install-7zip -downloadurl $downloadurl -installerPath $installerPath
 }
